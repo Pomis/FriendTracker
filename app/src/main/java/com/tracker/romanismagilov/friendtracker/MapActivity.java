3fa37a3b991604a0ed08d279619e06084eab7510
Pomis <pomis172@gmail.com>
2015-04-26 05:25:57 +0300

    VEDRA

--- app/src/main/java/com/tracker/romanismagilov/friendtracker/MapActivity.java
+++ app/src/main/java/com/tracker/romanismagilov/friendtracker/MapActivity.java
@@ -3,10 +3,23 @@ package com.tracker.romanismagilov.friendtracker;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class MapActivity extends Activity implements OnMapReadyCallback {
 
@@ -36,7 +49,43 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
         Toast.makeText(this, "ГОВНОЖОПА", Toast.LENGTH_LONG);
         //FriendTrackerAPI frapi = new FriendTrackerAPI();
         //frapi.AddUser(0L, null, 0F, 0F);
-        ((WebView) findViewById(R.id.secretwv)).loadUrl("http://friendtracker.esy.es/add_new_user.php?VKID="+LogInActivity.account.user_id+"&Name=test&PointX=1234&PointY=431");
+        //((WebView) findViewById(R.id.secretwv)).loadUrl("http://friendtracker.esy.es/add_new_user.php?VKID="+LogInActivity.account.user_id+"&Name=test&PointX=1234&PointY=431");
         //((WebView) findViewById(R.id.secretwv)).destroy();
+        new RequestTask().execute("http://friendtracker.esy.es/add_new_user.php?VKID=555666777&Name=test&PointX=1234&PointY=431");//здесь надыбал
+
+    }
+
+    /// THE SHITTA FUCKAAH
+    class RequestTask extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... uri) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+            try {
+                response = httpclient.execute(new HttpGet(uri[0]));
+                StatusLine statusLine = response.getStatusLine();
+                if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    out.close();
+                    responseString = out.toString();
+                } else {
+                    //Closes the connection.
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+            } catch (ClientProtocolException e) {
+            } catch (IOException e) {
+            }
+            return responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d("435678", "Response: " + result);
+        }
     }
 }
